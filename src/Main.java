9aba1bf6251af4658811a8b768266691c1ea99f3
Yeico17 <jacobo2250211@correo.uis.edu.co>
2026-09-10 23:02:37 -0500

    actualizacion main

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,53 @@
-void main() {
-    System.out.println("=== Prueba de ListStack ===");
-    ListStack listStack = new ListStack();
-    System.out.println("¿Está vacía? " + listStack.isEmpty());
+public class Main {
+    public static void main (String[] args){
+        System.out.println("=== Prueba de ListStack ===");
+        ListStack listStack = new ListStack();
+        System.out.println("¿Está vacía? " + listStack.isEmpty());
 
-    listStack.push(1);
-    listStack.push("Juan");
-    listStack.push(true);
-    System.out.println(listStack.toString());
-    System.out.println("Tamaño: " + listStack.getSize());
-    System.out.println("Peek: " + listStack.peek());
+        listStack.push(1);
+        listStack.push("Juan");
+        listStack.push(true);
+        System.out.println(listStack.toString());
+        System.out.println("Tamaño: " + listStack.getSize());
+        System.out.println("Peek: " + listStack.peek());
 
-    Object desapilado = listStack.pop();
-    System.out.println("Se desapiló: " + desapilado);
-    System.out.println(listStack.toString());
-    System.out.println("Tamaño: " + listStack.getSize());
-//termine
-    listStack.clear();
-    System.out.println("Después de clear: " + listStack.toString());
-    System.out.println("¿Está vacía? " + listStack.isEmpty());
+        Object desapilado = listStack.pop();
+        System.out.println("Se desapiló: " + desapilado);
+        System.out.println(listStack.toString());
+        System.out.println("Tamaño: " + listStack.getSize());
 
-    System.out.println();
-    System.out.println("=== Prueba de ArrayStack ===");
-    ArrayStack arrayStack = new ArrayStack(5);
-    System.out.println("¿Está vacía? " + arrayStack.isEmpty());
+        listStack.clear();
+        System.out.println("Después de clear: " + listStack.toString());
+        System.out.println("¿Está vacía? " + listStack.isEmpty());
 
-    arrayStack.push(1);
-    arrayStack.push("Juan");
-    arrayStack.push(true);
-    System.out.println(arrayStack.toString());
-    System.out.println("Tamaño: " + arrayStack.getSize());
-    System.out.println("Peek: " + arrayStack.peek());
+        System.out.println();
+        System.out.println("=== Prueba de ArrayStack ===");
+        ArrayStack arrayStack = new ArrayStack(5);
+        System.out.println("¿Está vacía? " + arrayStack.isEmpty());
 
-    Object desapiladoArray = arrayStack.pop();
-    System.out.println("Se desapiló: " + desapiladoArray);
-    System.out.println(arrayStack.toString());
-    System.out.println("Tamaño: " + arrayStack.getSize());
+        arrayStack.push(1);
+        arrayStack.push("Juan");
+        arrayStack.push(true);
+        System.out.println(arrayStack.toString());
+        System.out.println("Tamaño: " + arrayStack.getSize());
+        System.out.println("Peek: " + arrayStack.peek());
 
-    arrayStack.push("A");
-    arrayStack.push("B");
-    arrayStack.push("C");
-    System.out.println(arrayStack.toString());
-    System.out.println("¿Está llena? " + arrayStack.isFull());
-    boolean resultado = arrayStack.push("D");
-    System.out.println("¿Se pudo agregar 'D'? " + resultado);
+        Object desapiladoArray = arrayStack.pop();
+        System.out.println("Se desapiló: " + desapiladoArray);
+        System.out.println(arrayStack.toString());
+        System.out.println("Tamaño: " + arrayStack.getSize());
 
-    arrayStack.clear();
-    System.out.println("Después de clear: " + arrayStack.toString());
-    System.out.println("¿Está vacía? " + arrayStack.isEmpty());
+        arrayStack.push("A");
+        arrayStack.push("B");
+        arrayStack.push("C");
+        System.out.println(arrayStack.toString());
+        System.out.println("¿Está llena? " + arrayStack.isFull());
+        boolean resultado = arrayStack.push("D");
+        System.out.println("¿Se pudo agregar 'D'? " + resultado);
+
+        arrayStack.clear();
+        System.out.println("Después de clear: " + arrayStack.toString());
+        System.out.println("¿Está vacía? " + arrayStack.isEmpty());
+    }
+    
 }
